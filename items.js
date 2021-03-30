24da0038d2d3011aaeae38ff854d6bc367376674
AngelDOrtiz <aortizghs@gmail.com>
2021-03-30 16:57:56 -0700

    Done! (for now...)

--- items.js
+++ items.js
@@ -24,7 +24,7 @@ export const magicItem = [
         type: 'Ring',
         rarity: 'Rare',
         attunement: true,
-        description: 'While wearing the ring, you can use an action to expend 1 to 3 of its Charges to Attack one creature you can see within 60 feet of you.',
+        description: 'Use an action to expend 1 to 3 of its Charges to Attack one creature you can see within 60 feet of you.',
         color: 'skyblue',
         price: '8600',
         image: 'https://media-waterdeep.cursecdn.com/avatars/thumbnails/7/370/1000/1000/636284762552967774.jpeg'
@@ -44,7 +44,7 @@ export const magicItem = [
         type: 'Staff',
         rarity: 'Common',
         attunement: false,
-        description: 'While holding it, you can use an action to expend 1 charge from the staff and cause a flower to sprout from a patch of earth or soil within 5 feet of you, or from the staff itself.',
+        description: 'Use an action to cause a flower to sprout from a patch of earth or soil within 5 feet of you.',
         color: 'lightgray',
         price: '100',
         image: 'https://www.pngitem.com/pimgs/m/339-3392366_d-d-staff-of-flowers-hd-png-download.png'
