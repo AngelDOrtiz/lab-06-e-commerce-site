81c95e33c042a95299872a377936d4bf433be73b
AngelDOrtiz <aortizghs@gmail.com>
2021-04-01 14:18:00 -0700

    created dev branch and fixed site

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,31 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { findById } from '../utils.js';
+import { magicItem } from '../items.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('take in an array and return an item with matching ID', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = {
+        id: 1,
+        name: 'Mithral Armor',
+        type: 'Armor',
+        rarity: 'Uncommon',
+        attunement: false,
+        description: 'Mithral is a light, flexible metal. A mithral chain shirt or breastplate can be worn under normal clothes.',
+        color: 'lightgreen',
+        price: '500',
+        image: 'https://i.pinimg.com/originals/e3/4a/c4/e34ac4f1c0d929a9a71361ef5d267aab.jpg'
+    };
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = findById(magicItem, expected.id);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(actual, expected);
 });
